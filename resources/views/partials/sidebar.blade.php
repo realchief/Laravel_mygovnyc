dd6ba80f2e4f5b6d2b88b482054e12fefc516e4f
realchief <slobodan.topdev@gmail.com>
2018-10-24 12:21:35 +0200

    sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -2,12 +2,12 @@
 	<img src="../images/logo.png" class="img-responsive">
 	<nav class="mdl-navigation">
 		<a class="mdl-navigation__link" href="/">Home</a>
-		<a class="mdl-navigation__link" href="/organizations">Agencies & Departments</a>
+		<a class="mdl-navigation__link" href="/organizations">Organizations</a>
 		<a class="mdl-navigation__link" href="/services">Services</a>
 		<a class="mdl-navigation__link" href="/projects">Projects</a>
 		<a class="mdl-navigation__link" href="/data">Data</a>
 		<a class="mdl-navigation__link" href="/laws">Charter & Code</a>
 		<a class="mdl-navigation__link" target="_blank" href="http://mygov.nyc/about/">About</a>
-		<a class="mdl-navigation__link" target="_blank" href="http://mygov.nyc/get-involved/">Get Invoted</a>
+		<a class="mdl-navigation__link" target="_blank" href="http://mygov.nyc/get-involved/">Get Involved</a>
 	</nav>
 </div>
